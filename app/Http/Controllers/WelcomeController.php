c0da53e266a6b2f54d943a735b8df377be2047ef
lukacg <lukac91@gmail.com>
2019-11-20 15:51:01 +0100

    nn

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -13,6 +13,12 @@ class WelcomeController extends Controller
 {
     public function index(Request $request)
     {
+        $companies = Companies::paginate(5);
+        $devices = Device::paginate(5);
+        $vehicles = Vehicle::paginate(5);
+        $drivers = Driver::paginate(5);
+
+
         $items = [];
         foreach (Companies::all() as $comp) $items[]['company']=$comp;
         foreach (Device::all() as $index => $dev){
@@ -29,8 +35,43 @@ class WelcomeController extends Controller
             if($items[$index]) $items[$index]['driver']=$dr;
             else $items[]['driver']=$dr;
         }
+
+        
+
         return view('/welcome', ['items'=>$items]);
     }
 
- 
+
+
+    /*
+    public function items(Request $request)
+    {
+        $items = [];
+        foreach (Companies::all() as $comp) $items[]['company']=$comp;
+        foreach (Device::all() as $index => $dev){
+            if($items[$index]) $items[$index]['device']=$dev;
+            else $items[]['device']=$dev;
+        }
+
+        foreach (Vehicle::all() as $index => $veh){
+            if($items[$index]) $items[$index]['vehicle']=$veh;
+            else $items[]['vehicle']=$veh;
+        }
+
+        foreach (Driver::all() as $index => $dr){
+            if($items[$index]) $items[$index]['driver']=$dr;
+            else $items[]['driver']=$dr;
+        }
+
+        $currentPage = LengthAwarePaginator::resolveCurrentPage();
+        $itemCollection = collect($items);
+        $perPage = 1;
+        $currentPageItems = $itemCollection->slice(($currentPage * $perPage) - $perPage, $perPage)->all();
+        $paginatedItems = new LengthAwarePaginator($currentPageItems , count($itemCollection), $perPage);
+        $paginatedItems->setPath($request->url());
+
+        return view('/welcome', ['items'=>$paginatedItems]);
+    }
+
+  */
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Collection;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,33 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /**
+         * Paginate a standard Laravel Collection.
+         *
+         * @param int $perPage
+         * @param int $total
+         * @param int $page
+         * @param string $pageName
+         * @return array
+         */
+
+        Collection::macro('paginate', function($perPage, $total = null, $page = null, $pageName = 'page') 
+
+        {
+            $page = $page ?: LengthAwarePaginator::resolveCurrentPage($pageName);
+            return new LengthAwarePaginator(
+                $this->forPage($page, $perPage),
+                $total ?: $this->count(),
+                $perPage,
+                $page,
+
+                [
+                    'path' => LengthAwarePaginator::resolveCurrentPath(),
+                    'pageName' => $pageName,
+                ]
+
+            );
+        }
+        );       
     }
 }
