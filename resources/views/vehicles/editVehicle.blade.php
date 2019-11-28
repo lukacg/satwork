3044a4da0c978620a1b1fdb065f93815a82b0f2c
lukacg <lukac91@gmail.com>
2019-11-28 15:50:42 +0100

    "device edit modal (not finished)"

--- resources/views/vehicles/editVehicle.blade.php
+++ resources/views/vehicles/editVehicle.blade.php
@@ -1,63 +1,70 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    <title>Edit vehicle</title>
-</head>
-<body>
+<div class="form-group">
+    <label for="vehicle_type">Type</label>
+    <input type="text" class="form-controll" name="vehicle_type" id="vehicle_type" autofocus>
+</div>
 
-    <div class="containter">
-        <div class="row">
+<div class="form-group">
+    <label for="model">Model</label>
+    <input type="text" class="form-controll" name="model" id="model">
+</div>
 
-            <div class="col-3">
-            </div>
-        
-                <div class="col-6">
-                <div class="container">
-                    <br><br>
-                        <button type="button"><a href="/vehicles">Back</button></a> 
+<div class="form-group">
+    <label for="production year">Production year</label>
+    <input type="number" min="1980" max="2030" step="1" class="form-controll" name="production_year" id="production_year">
+</div>
 
-                        <form id="contact" action="{{route('updatevehicle', $vehicle->id)}}" method="POST">
-                        {{csrf_field()}}
-           
-                        <h3>Edit vehicle</h3>
-                        Type:<br><input placeholder="Type"  type="text" name ="vehicle_type" value="{{$vehicle->vehicle_type}}" tabindex="1"  autofocus>
-                        </fieldset>
-                        <fieldset>
-                        Model:<br><input placeholder="Model" type="text" name="model" value="{{$vehicle->model}}" tabindex="2" autofocus>
-                        </fieldset>
-                        <fieldset>
-                        Production year:<br><input placeholder="Production year" type="number" min="1980" max="2030" step="1" name="production_year" value="{{$vehicle->production_year}}" tabindex="3" >
-                        </fieldset>
-                        <fieldset>
-                        License plate:<br><input placeholder="License plate" type="text" name="license_plate" value="{{$vehicle->license_plate}}" tabindex="4">
-                        </fieldset>
-                        
-                        <fieldset>
-                        <span>Device</span>
-                        <select name="deviceId">
-                        @foreach($devices as $dev)
-                            <option value="{{$dev->id}}" @if ($dev->id == $vehicle->companyId) selected @endif>{{$dev->device_type}}</option>
-                        @endforeach
-                        </select>
-                        </fieldset>
-                        <br>
-                        <fieldset>
-                            <button name="submit" type="submit" id="contact-submit" class="btn btn-primary" data-submit="...Sending">Update</button>
-                        </fieldset>
-                        </form>
-                </div>
+<div class="form-group">
+    <label for="license_plate">License plate</label>
+    <input type="text" class="form-controll" name="license_plate" id="license_plate">
+</div>
 
-                    <div class="col-3">
-                    </div>
-                </div>
-        </div>
+<div class="form-group">
+    <span>Device</span>
+    <select name="deviceId" id=deviceId>
+        @foreach($devices as $dev)
+        <option value="{{$dev->id}}" @if ($dev->id == $vehicle->companyId) selected @endif>{{$dev->device_type}}</option>
+        @endforeach
+    </select>
+</div>
+
+<!-- <form id="contact" action="{{route('updatevehicle', $vehicle->id)}}" method="POST">
+    {{csrf_field()}}
+
+    <h3>Edit vehicle</h3>
+    Type:<br><input placeholder="Type" type="text" name="vehicle_type" value="{{$vehicle->vehicle_type}}" tabindex="1" autofocus>
+    </fieldset>
+    <fieldset>
+        Model:<br><input placeholder="Model" type="text" name="model" value="{{$vehicle->model}}" tabindex="2" autofocus>
+    </fieldset>
+    <fieldset>
+        Production year:<br><input placeholder="Production year" type="number" min="1980" max="2030" step="1" name="production_year" value="{{$vehicle->production_year}}" tabindex="3">
+    </fieldset>
+    <fieldset>
+        License plate:<br><input placeholder="License plate" type="text" name="license_plate" value="{{$vehicle->license_plate}}" tabindex="4">
+    </fieldset>
+
+    <fieldset>
+        <span>Device</span>
+        <select name="deviceId">
+            @foreach($devices as $dev)
+            <option value="{{$dev->id}}" @if ($dev->id == $vehicle->companyId) selected @endif>{{$dev->device_type}}</option>
+            @endforeach
+        </select>
+    </fieldset>
+    <br>
+    <fieldset>
+        <button name="submit" type="submit" id="contact-submit" class="btn btn-primary" data-submit="...Sending">Update</button>
+    </fieldset>
+    </form>
     </div>
-</body>
 
-</html>
+    <div class="col-3">
+    </div>
+    </div>
+    </div>
+    </div>
+    </body>
+
+    </html>
+
+    -->
--- resources/views/vehicles/vehicles.blade.php
+++ resources/views/vehicles/vehicles.blade.php
@@ -38,7 +38,15 @@ Vehicles
                     <td>{{$vehicle->device->device_type}}</a>
 
                     <td>
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editModal">Edit</button>
+                        <button type="button" class="btn btn-primary" 
+
+                        data-vehicle.vehicle_type="{{$vehicle->vehicle_type}}" 
+                        data-vehicle.model="{{$vehicle->model}}" 
+                        data-vehicle.production_year="{{$vehicle->production_year}}" 
+                        data-vehicle.license_plate="{{$vehicle->license_plate}}" 
+                        data-device_type="{{$vehicle->device->device_type}}" 
+                        
+                        data-toggle="modal"  data-target="#editModal">Edit</button> <!-- data-"name" = button.data("name")  in script-->
                     </td>
 
                     <td><a onclick="return confirm('Are You sure?')" href="deleteVehicle/{{$vehicle->id}}"><button>Delete</button></a></td>
@@ -47,28 +55,47 @@ Vehicles
             </tbody>
         </table>
     </div>
-    
+
     <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Vehicle edit</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h5 class="modal-title" id="myModalLabel">Vehicle edit</h5>
+                </div>
+                <form action="{{route('updatevehicle', $vehicle->id)}}" method="POST">
+                    {{csrf_field()}}
                     <div class="modal-body">
                         @include('vehicles.editVehicle')
-
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
                     </div>
-                </div>
-
             </div>
-
         </div>
-    
+    </div>
+
+    <script>
+        $('#editModal').on('show.bs.modal', function(event) {
+            var button = $(event.relatedTarget) 
+            var vehicle_type = button.data('vehicle.vehicle_type') 
+            var model = button.data('vehicle.model') 
+            var production_year = button.data('vehicle.production_year') 
+            var license_plate = button.data('vehicle.license_plate') 
+            var device_type = button.data('device_type') 
+
+
+            var modal = $(this)
+            modal.find('.modal-body #vehicle_type').val(vehicle_type)        //.modal-body #id from edit
+            modal.find('.modal-body #model').val(model)
+            modal.find('.modal-body #production_year').val(production_year)
+            modal.find('.modal-body #license_plate').val(license_plate)
+            modal.find('.modal-body #dev').val(deviceId)
+
+             
+        })
+    </script>
+
 
     @endsection
